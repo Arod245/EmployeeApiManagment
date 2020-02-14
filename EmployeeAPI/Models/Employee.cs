2b01a7fb71f56f6262174c80f6bc73b9032a8c06
Antonio Rodrigues <arodriguez245@cnm.edu>
2020-02-14 15:10:14 -0700

    Add project files.

---
+++ EmployeeAPI/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeAPI.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+        public string Position { get; set; }
+        public double Salary { get; set; }
+    }
+}
