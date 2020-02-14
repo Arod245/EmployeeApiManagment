10c882c4d11986ea15004c8fe4b445f62c288dab
Antonio Rodrigues <arodriguez245@cnm.edu>
2020-02-14 15:14:27 -0700

    Added Api request class

---
+++ EmployeeWPF/APIHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWPF
+{
+    public static class APIHelper
+    {
+        public static HttpClient client { get; set; }
+
+        static APIHelper()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44386/");
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+        }
+    }
+}
